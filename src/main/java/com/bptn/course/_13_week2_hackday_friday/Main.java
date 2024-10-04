b434e2bb77d370d87ec89b1f766a0f9be66f8209
Moti <motiadd@gmail.com>
2024-10-04 14:52:05 -0400

    Initial Commit

---
+++ src/main/java/com/bptn/course/_13_week2_hackday_friday/Main.java
@@ -0,0 +1,12 @@
+package com.bptn.course._13_week2_hackday_friday;
+
+//Do not modify below code
+public class Main {
+ public static void main(String[] args) {
+     // create an instance of Employee class and initialize it with salary = 100 and hoursPerDay = 8
+     Employee student = new Employee(100, 8);
+     
+     // print the salary of the employee
+     System.out.println(student.getSalary());
+ }
+}
